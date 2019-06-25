05ccbbb8956028d706daedf170af343549826900
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-25 11:14:04 -0300

    Criando classes css e bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <html>
-<h1>Bem Vindo!</h1>
+<head>
+    <title>Minha Loja</title>
+    <meta charset="uft-8">
+    <link rel="stylesheet" href="css/bootsrap.css">
+    <link rel="stylesheet" href="css/loja.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="principal">
+            <h1>Bem Vindo!</h1>
+        </div>
+    </div>
+        
+</body>
 </html>
