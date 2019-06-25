caaf360d5d05c4d115c7edd05f2fbbf64e0748fd
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-25 14:58:47 -0300

    Criando conexão com o banco de dados e inserindo dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,6 +3,17 @@
 <?php
     $nome = $_GET["nome"]; //$_GET pega o valor do parâmetro
     $preco = $_GET["preco"];
+
+    //Conecta no mysql passando o ip, usuário, senha e nome do banco
+    // e nos devolve a conexão com o banco de dados. (ABRE A CONEXÃO)
+    $conexao = mysqli_connect('localhost', 'root', '', 'loja'); 
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+    //Executa a query passando em qual conexão e qual query
+    mysqli_query($conexao, $query);
+
+    //Depois de executar fecha a conexão
+    mysqli_close($conexao);
 ?>
 
 <p class="alert-sucess">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php include("cabecalho.php");?>
             <h1>Bem Vindo!</h1>
 <?php include("rodape.php");?>
-<?php 
-
-    $numeros = array(1,2,3,4,5,6);
-
-    calcularArray($numeros);
-
-    function calcularArray($numeros) {
-
-        $soma = 0;
-        for ($i = 0; $i < sizeof($numeros); $i++) {
-            $soma = $soma + $numeros[$i];
-        }
-
-        return $soma;
-
-    }
-
-    $soma = calcularArray($numeros);
-    echo $soma;
-?>
