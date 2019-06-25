3334b86b7cd3296a785f488d35489eb0e48eff04
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-25 14:27:16 -0300

    função para calcular soma de array e mostrar na tela

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php include("cabecalho.php");?>
             <h1>Bem Vindo!</h1>
 <?php include("rodape.php");?>
+<?php 
+
+    $numeros = array(1,2,3,4,5,6);
+
+    calcularArray($numeros);
+
+    function calcularArray($numeros) {
+
+        $soma = 0;
+        for ($i = 0; $i < sizeof($numeros); $i++) {
+            $soma = $soma + $numeros[$i];
+        }
+
+        return $soma;
+
+    }
+
+    $soma = calcularArray($numeros);
+    echo $soma;
+?>
