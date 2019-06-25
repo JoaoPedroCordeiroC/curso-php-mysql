3769ce49d447f3da29f38e79b8900f86ec9a164e
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-25 11:05:13 -0300

    Fazendo requisição do formulário para adiciona-produto

---
+++ produto-formulario.php
@@ -0,0 +1,10 @@
+<html>
+<form action="adiciona-produto.php">
+    Nome:
+    <input type="text" name="nome"><br>
+    Preco:
+    <input type="text" name="preco"><br>
+    
+    <input type="submit" value="Castrar">
+</form>
+</html>
