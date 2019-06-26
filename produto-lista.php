ab7ef1546e75bb9416a8cddf39f854a150c2ecf6
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-26 08:05:52 -0300

    Melhorando classe produto-lista

--- produto-lista.php
+++ produto-lista.php
@@ -1,13 +1,24 @@
 <?php include("cabecalho.php");?>
 <?php include("conecta.php");
 
-$resultado = mysqli_query($conexao, "select * from produtos");
+function listaProdutos($conexao) {
+    $produtos = array();
+    $resultado = mysqli_query($conexao, "select * from produtos");
 
     // Pega o produto associado a este resultado
-while($produto = mysqli_fetch_assoc($resultado)) {
-    echo $produto['nome'] . "<br/>";
+    while($produto = mysqli_fetch_assoc($resultado)) {
+        array_push($produtos, $produto);
+
+    }
+    return $produtos;
 }
 
+
+$produtos = listaProdutos($conexao);
+
+foreach($produtos as $produto) {
+    echo $produto['nome'] . "<br/>";
+}
 
 ?>
 
