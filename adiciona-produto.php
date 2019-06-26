1fc75cc134efb466526ae75b93694e7ce895dc26
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-26 11:55:05 -0300

    Trocando GET para POST

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,15 +3,15 @@ include("conecta.php");
 include("banco-produto.php");
     
 
-    $nome = $_GET["nome"]; //$_GET pega o valor do parâmetro
-    $preco = $_GET["preco"];
-        
-    
-    //Executa a query passando em qual conexão e qual query
-    if(insereProduto($conexao, $nome, $preco)) { ?>
-        <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> foi adicionado com sucesso!</p>
+        $nome = $_POST["nome"]; //$_GET pega o valor do parâmetro
+        $preco = $_POST["preco"];
+        $descricao = $_POST["descricao"];
+
+        //Executa a query passando em qual conexão e qual query
+        if(insereProduto($conexao, $nome, $preco, $descricao)) { ?>
+                <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> foi adicionado com sucesso!</p>
 <?php   } else { 
-        $msg = mysqli_error($conexao);
+                $msg = mysqli_error($conexao);
 ?>
         <p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg?></p>
 <?php
--- produto-formulario.php
+++ produto-formulario.php
@@ -1,6 +1,6 @@
 <?php include("cabecalho.php");?>
     <h1>Formulario de produto</h1>
-<form action="adiciona-produto.php">
+<form action="adiciona-produto.php" method="post">
     <table>
         <tr>
             <td>Nome:</td> 
@@ -8,7 +8,11 @@
         </tr>
         <tr>
             <td>Preco:</td>
-            <td><input class="form-control" type="text" name="preco"><br></td>
+            <td><input class="form-control" type="number" name="preco"><br></td>
+        </tr>
+        <tr>
+            <td>Descrição:</td>
+            <td><textarea name="descricao" class="form-control"></textarea></td>
         </tr>
         <tr>
             <td>
