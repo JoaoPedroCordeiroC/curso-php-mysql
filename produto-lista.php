f3fe6cccddf7923dd75f9429c5cf80d51711883a
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-26 09:41:27 -0300

    Criando botão e link remover na lista de produtos

--- produto-lista.php
+++ produto-lista.php
@@ -13,6 +13,9 @@
     <tr>
         <td><?= $produto['nome'] ?></td>
         <td><?= $produto['preco'] ?></td>
+        <td>
+            <a href="remove-produto.php?id<?=$produto['id']?>" class="btn btn-danger">remover</a>
+        </td>
     </tr>
     <?php
         endforeach
