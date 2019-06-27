3ce26e8069a4cca49e744412b851abc92d7550d1
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-27 17:06:06 -0300

    Usando sessão e criando logout

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
-<?php include("cabecalho.php");?>
+<?php include("cabecalho.php");
+include("logica-usuario.php");
+?>
+<?php if(isset($_GET["logout"]) && $_GET["logout"]==true) { ?>
+    <p class="alert-success">Deslogado com sucesso!</p>
+<?php }?>
 <?php if(isset($_GET["login"]) && $_GET["login"]==true) { ?>
     <p class="alert-success">Logado com sucesso!</p>
 <?php }?>
 <?php if(isset($_GET["login"]) && $_GET["login"]==false) { ?>
     <p class="alert-danger">Usuário ou senha inválido!</p>
 <?php }?>
+<?php if(isset($_GET["falhaDeSeguranca"]) && $_GET["falhaDeSeguranca"]==true) { ?>
+    <p class="alert-danger">Você não tem acesso a essa funcionalidade!</p>
+<?php }?>
 
     <h1>Bem Vindo!</h1>
 
-<?php if(isset($_COOKIE["usuario_logado"])) {?>
-    <p class="text-success">Você está logado como <?=$_COOKIE["usuario_logado"] ?>.</p>
+<?php if(usuarioEstaLogado()) {?>
+    <p class="text-success">Você está logado como <?=usuarioLogado()?>.
+    <a href="logout.php">Deslogar</a></p>
 <?php } else {?>
     <h2>Login</h2>
     <form action="login.php" method="post">
