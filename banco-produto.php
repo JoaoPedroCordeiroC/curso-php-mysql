4abbe7afeab5ec8438e47ddec03bc50df1d71f6f
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-27 08:56:53 -0300

    Fim do projeto.

--- banco-produto.php
+++ banco-produto.php
@@ -9,6 +9,7 @@ function listaProdutos($conexao) {
         array_push($produtos, $produto);
 
     }
+    
     return $produtos;
 }
 
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php include("cabecalho.php");?>
             <h1>Bem Vindo!</h1>
+            <br>
+            <h2>Esta é a minha primeira aplicação web com PHP</h2>
 <?php include("rodape.php");?>
